5e254aebd6ea202602ae1113ee8b3333e59eee12
Ann Root <ann.root@bvsd.org>
2025-02-24 11:18:27 -0700

    Create ArrayListStart.java

---
+++ ArrayListStart.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ArrayListStart {
+
+  public static void main(String[] args) {
+
+          System.out.println("ArrayList Assignment 1");
+
+
+          System.out.println("ArrayList Assignment 2");
+
+
+         System.out.println("ArrayList Assignment 3");
+  }
+}
